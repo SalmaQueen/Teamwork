2729ada3cbbf4b132a88e736550b1aef1e83b7ec
Agnes NM <agnesndutamwaniki@gmail.com>
2019-11-08 15:31:33 +0300

    edit test

--- app/admin_sign_in.js
+++ app/admin_sign_in.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req,res)=>{
+    req.params.username;
+    req.params.password;
     res.send("admin logged in successfully!");
     app.listen(3000);
 });

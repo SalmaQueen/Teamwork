3c1f509b80de9070056418e7654e6e46c46811e3
Agnes NM <agnesndutamwaniki@gmail.com>
2019-11-09 10:32:29 +0300

    create employee can edit their article test

---
+++ app/emp_edit_articles.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.put("/", (req,res) => {
+    req.params.articleTitle;
+    req.params.articleId;
+    req.params.articleAuthor;
+    req.params.authorId;
+    res.send("your changes have been successfully saved");
+    app.listen(3000);
+});
---
+++ spec/emp_edit_articles_spec.js
@@ -0,0 +1,42 @@
+const request = require("request");
+const base_url = "http://localhost:3000/";
+ 
+ 
+describe("employee can edit their articles", () => {
+    describe("PUT /", () => {
+        it("return a response status code of 200", () => {
+            request.get(base_url,(error,response,body) =>{
+                expect(response.statusCode).toBe(200);
+                done();
+            });
+        });
+        it("return a successful message body", () => {
+            request.get(base_url, (error, response, body) => {
+                expect(body).toBe('changes saved');
+                done();
+            });
+        });
+        it("send message body with article title", () => {
+            request.post(base_url, (error,request,body) => {
+                expect(body).toContain(gifTitle);
+                done();
+            });
+        });
+        it("send message body with article id", () => {
+            request.post(base_url, (error, request,body) => {
+                expect(body).toContain(articleId);            });
+    
+        });
+        it("send message body with article author", () => {
+                request.post(base_url, (error, request,body) => {
+                    expect(body).toContain(articleAuthor);
+                });        
+         });
+        it("send message body with author id", () => {
+                        request.post(base_url, (error, request,body) => {
+                            expect(body).toContain(authorId);
+                        });        
+                 });
+        });
+});
+

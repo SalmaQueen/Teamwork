a824fdbe3bffd224db8918a9b9552a48851dd14e
Agnes NM <agnesndutamwaniki@gmail.com>
2019-11-09 13:56:14 +0300

    add admin can delete a flagged article, gif or comment

--- app/admin_del_comment_article_gif_flagged.js
+++ app/admin_del_comment_article_gif_flagged.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req,res) => {
+app.delete("/", (req,res) => {
     req.params.articleId;
     req.params.gifId;
-    req.params.commentId;  
+    req.params.commentId; 
+    req.params.articleFlagId;
+    req.params.gifFlagId;
+    req.params.commentFlagId;
     res.json(200);
     app.listen(3000);
 });
--- spec/admin_del_article_comment_gif_flagged_spec.js
+++ spec/admin_del_article_comment_gif_flagged_spec.js
@@ -2,15 +2,36 @@ const request = require("request");
 const base_url = "http://localhost:3000/";
  
  
-describe("employee can flag gifs, comments and articles as inappropriate", () => {
-    describe("GET /", () => {
+describe("admin can delete flagged gifs, comments and articles as inappropriate", () => {
+    describe("DELETE /", () => {
         it("return a response status code of 200", () => {
             request.get(base_url,(error,response,body) =>{
                 expect(response.statusCode).toBe(200);
                 done();
             });
         });
+
+        it("return a sucessful response message", () => {
+            request.get(base_url,(error,response,body) =>{
+                expect(response.statusCode).toBe('gif deleted');
+                done();
+            });
+        });
+
+        it("return a sucessful response message", () => {
+            request.get(base_url,(error,response,body) =>{
+                expect(response.statusCode).toBe('comment deleted');
+                done();
+            });
+        });
     
+        it("return a sucssful response message", () => {
+            request.get(base_url,(error,response,body) =>{
+                expect(response.statusCode).toBe('article deleted');
+                done();
+            });
+        });
+
         it("send message body with gif id", () => {
             request.post(base_url, (error,request,body) => {
                 expect(body).toContain(gifId);

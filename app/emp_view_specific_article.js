50d7c54e61c3996715e58dde173ef1957668c8c9
Agnes NM <agnesndutamwaniki@gmail.com>
2019-11-09 13:09:27 +0300

    add employee can view specific article test

--- app/emp_view_specific_article.js
+++ app/emp_view_specific_article.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req,res) => {
-    req.params.gifTitle;
-    req.params.gifId;  
+    req.params.articleTitle;
+    req.params.articleId;  
     res.json(200);
     app.listen(3000);
 });
--- spec/emp_view_specific_article_spec.js
+++ spec/emp_view_specific_article_spec.js
@@ -2,7 +2,7 @@ const request = require("request");
 const base_url = "http://localhost:3000/";
  
  
-describe("employee can view a specific gif", () => {
+describe("employee can view a specific article", () => {
     describe("GET /", () => {
         it("return a response status code of 200", () => {
             request.get(base_url,(error,response,body) =>{
@@ -11,16 +11,16 @@ describe("employee can view a specific gif", () => {
             });
         });
     
-        it("send message body with gif title", () => {
+        it("send message body with article title", () => {
             request.post(base_url, (error,request,body) => {
-                expect(body).toContain(gifTitle);
+                expect(body).toContain(articleTitle);
                 done();
             });
         });
 
-        it("send message body with gif id", () => {
-            request.post(base_url, (errempor, request,body) => {
-                expect(body).toContain(gifId);
+        it("send message body with article id", () => {
+            request.post(base_url, (error, request,body) => {
+                expect(body).toContain(articleId);
             });
         });
        

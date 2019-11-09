e65dca44ee65d6c1999a7fc49284933f766bfa14
Agnes NM <agnesndutamwaniki@gmail.com>
2019-11-09 09:29:05 +0300

    add share gif test

--- app/emp_create_articles.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.post("/", (req,res) => {
-    req.params.articleTitle; 
-    req.params.articleAuthor;     
-    res.json('article created successfully!');
-    app.listen(3000);
-});

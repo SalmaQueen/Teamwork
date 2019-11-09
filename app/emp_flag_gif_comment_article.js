4be3906fd98fea191f0da70722a9cbb22b7fbe8a
Agnes NM <agnesndutamwaniki@gmail.com>
2019-11-09 13:34:41 +0300

    add employee can flag a comment, gif or article as inappropriate test

--- app/emp_flag_gif_comment_article.js
+++ app/emp_flag_gif_comment_article.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req,res) => {
-    req.params.article;
-    req.params.articleCategory;  
+    req.params.articleId;
+    req.params.gifId;
+    req.params.commentId;  
     res.json(200);
     app.listen(3000);
 });
--- spec/emp_flag_gif_comment_article_spec.js
+++ spec/emp_flag_gif_comment_article_spec.js
@@ -2,7 +2,7 @@ const request = require("request");
 const base_url = "http://localhost:3000/";
  
  
-describe("employee can view all articles in a category", () => {
+describe("employee can flag gifs, comments and articles as inappropriate", () => {
     describe("GET /", () => {
         it("return a response status code of 200", () => {
             request.get(base_url,(error,response,body) =>{
@@ -11,16 +11,22 @@ describe("employee can view all articles in a category", () => {
             });
         });
     
-        it("send message body with article", () => {
+        it("send message body with gif id", () => {
             request.post(base_url, (error,request,body) => {
-                expect(body).toContain(article);
+                expect(body).toContain(gifId);
                 done();
             });
         });
 
-        it("send message body with article category", () => {
+        it("send message body with article id", () => {
             request.post(base_url, (error, request,body) => {
-                expect(body).toContain(articleCategory);
+                expect(body).toContain(articleId);
+            });
+        });
+
+        it("send message body with comment id", () => {
+            request.post(base_url, (error, request,body) => {
+                expect(body).toContain(commentId);
             });
         });
        

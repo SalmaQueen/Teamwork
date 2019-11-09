2f5a009f942119ce5a468d8d424fb3809d8a4cc5
Agnes NM <agnesndutamwaniki@gmail.com>
2019-11-09 14:17:57 +0300

    solve conflicts from merged feature branches

--- spec/admin_del_article_comment_gif_flagged_spec.js
+++ spec/admin_del_article_comment_gif_flagged_spec.js
@@ -2,36 +2,31 @@ const request = require("request");
 const base_url = "http://localhost:3000/";
  
  
-<<<<<<< HEAD:spec/admin_del_article_comment_gif_flagged_spec.js
 describe("admin can delete flagged gifs, comments and articles as inappropriate", () => {
-    describe("DELETE /", () => {
-        it("return a response status code of 200", () => {
-=======
-describe("employee can flag gifs, comments and articles as inappropriate", () => {
-    describe("POST /", () => {
+    describe("DELETE /", () => {        
         it("return a successful response status code of 200", () => {
->>>>>>> emp_flag_gif_comment_article:spec/emp_flag_gif_comment_article_spec.js
             request.get(base_url,(error,response,body) =>{
                 expect(response.statusCode).toBe(200);
                 done();
             });
         });
-<<<<<<< HEAD:spec/admin_del_article_comment_gif_flagged_spec.js
 
         it("return a sucessful response message", () => {
             request.get(base_url,(error,response,body) =>{
                 expect(response.statusCode).toBe('gif deleted');
-=======
+                done();
+            });
+        });
+        
+            
         it("return a successful response message", () => {
             request.get(base_url,(error,response,body) =>{
                 expect(response.body).toBe('gif flagged');
->>>>>>> emp_flag_gif_comment_article:spec/emp_flag_gif_comment_article_spec.js
                 done();
             });
         });
 
-<<<<<<< HEAD:spec/admin_del_article_comment_gif_flagged_spec.js
-        it("return a sucessful response message", () => {
+        it("return a successful response message", () => {
             request.get(base_url,(error,response,body) =>{
                 expect(response.statusCode).toBe('comment deleted');
                 done();
@@ -41,7 +36,11 @@ describe("employee can flag gifs, comments and articles as inappropriate", () =>
         it("return a sucssful response message", () => {
             request.get(base_url,(error,response,body) =>{
                 expect(response.statusCode).toBe('article deleted');
-=======
+            });
+
+        });
+
+
         it("return a successful response message", () => {
             request.get(base_url,(error,response,body) =>{
                 expect(response.body).toBe('article flagged');
@@ -52,7 +51,6 @@ describe("employee can flag gifs, comments and articles as inappropriate", () =>
         it("return a successful response message", () => {
             request.get(base_url,(error,response,body) =>{
                 expect(response.body).toBe('comment flagged');
->>>>>>> emp_flag_gif_comment_article:spec/emp_flag_gif_comment_article_spec.js
                 done();
             });
         });

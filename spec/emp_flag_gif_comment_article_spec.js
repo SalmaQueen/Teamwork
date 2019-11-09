5797bf6ac91ae5707fff0001dfc89857a3cdd940
Agnes NM <agnesndutamwaniki@gmail.com>
2019-11-09 14:03:02 +0300

    edit test

--- spec/emp_flag_gif_comment_article_spec.js
+++ spec/emp_flag_gif_comment_article_spec.js
@@ -4,13 +4,33 @@ const base_url = "http://localhost:3000/";
  
 describe("employee can flag gifs, comments and articles as inappropriate", () => {
     describe("GET /", () => {
-        it("return a response status code of 200", () => {
+        it("return a successful response status code of 200", () => {
             request.get(base_url,(error,response,body) =>{
                 expect(response.statusCode).toBe(200);
                 done();
             });
         });
-    
+        it("return a successful response message", () => {
+            request.get(base_url,(error,response,body) =>{
+                expect(response.body).toBe('gif flagged');
+                done();
+            });
+        });
+
+        it("return a successful response message", () => {
+            request.get(base_url,(error,response,body) =>{
+                expect(response.body).toBe('article flagged');
+                done();
+            });
+        });
+
+        it("return a successful response message", () => {
+            request.get(base_url,(error,response,body) =>{
+                expect(response.body).toBe('comment flagged');
+                done();
+            });
+        });
+
         it("send message body with gif id", () => {
             request.post(base_url, (error,request,body) => {
                 expect(body).toContain(gifId);

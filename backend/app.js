55893fcd9c66625a0143ccf2c754129763fcf736
Agnes NM <agnesndutamwaniki@gmail.com>
2019-11-15 18:28:50 +0300

    create files

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const createArticlesRoutes = require('./routes/create_articles');
+
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use('/api/v1/create_article', createArticlesRoutes);
+
+module.exports = app;
+

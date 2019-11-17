107b8b98f66eea423ce990aa760bfa2609171a29
Agnes NM <agnesndutamwaniki@gmail.com>
2019-11-17 07:51:06 +0300

    create admin can create employee account endpoint

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const createEmployeeAcctRoutes = require('./routes/create_emp_acct');
+
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use('/api/v1/create_emp_acct', createEmployeeAcctRoutes);
+
+
+module.exports = app;
---
+++ backend/routes/create_emp_acct.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+
+const { Pool} = require('pg');
+const connectionString = 'postgres://nduta:e1234f@localhost:5432/Teamwork';
+
+const pool = new Pool({
+    connectionString: connectionString,
+  });
+
+//post route
+router.post('/', (req, res, next) => {
+    const values = [   
+      req.body.first_name,
+      req.body.last_name,
+      req.body.email,
+      req.body.password,      
+      req.body.gender,
+      req.body.job_role,
+      req.body.department,
+      req.body.address,   
+      req.body.emp_id,   
+      req.body.db_role
+    ];                    
+  
+  const text = 'INSERT INTO employees(first_name,last_name,email,password, gender, job_role, department, address, emp_id, db_role) VALUES($1,$2,$3,$4,$5, $6, $7, $8, $9, $10) RETURNING *'; 
+
+  pool.query(text,values, (err,res) => {
+      if(err){
+        console.log(err.stack);
+        res.status(400);
+      }  else{
+        console.log(res.rows[0]);
+        
+      }   
+  });
+  
+});
+
+module.exports = router;
